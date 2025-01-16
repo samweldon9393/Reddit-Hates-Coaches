12253784f4aeb7e0249cdf7c90695265e85b10aa
Sam <samweldon9393@gmail.com>
2025-01-15 21:46:33 -0500

    print a bunch of stats in stats.py

--- stats.py
+++ stats.py
@@ -1,10 +1,53 @@
 import numpy as np 
 import pandas as pd
 import sqlite3
+import matplotlib.pyplot as plt
 
+#turn the SQL database into a pandas matrix to perform calculations
 conn = sqlite3.connect('coaches.db')
 cursor = conn.cursor()
 query = 'SELECT * FROM aggregated'
 df = pd.read_sql_query(query, conn)
+df.columns = ['Coach', 'Sentiment', 'Ratio', 'Positive', 'Negative']
 
-print(df)
+#get the variance in the data set 
+#find the distance of each ratio from the mean, save that in an array, square
+#sum those and divide by number of data points to get variance
+
+'''
+ratio = np.array(df['Ratio'])
+dists = ratio - np.mean(ratio)
+sq_dists = dists ** 2
+sum_sq_dists = sum(sq_dists)
+variance = sum_sq_dists / len(df)
+print(variance)
+'''
+
+#using built in functions
+variance = np.var(df['Ratio'])
+std = np.std(df['Ratio'])
+
+
+print('Standard Deviation: ' + str(std))
+print('variance: ' + str(variance))
+
+print('\n\n')
+
+
+iqr = np.quantile(df['Ratio'], .75) - np.quantile(df['Ratio'], .25)
+print('IQR: ' + str(iqr))
+
+lower = np.quantile(df['Ratio'], .25) - 1.5 * iqr
+upper = np.quantile(df['Ratio'], .75) + 1.5 * iqr
+
+print('lower: %s upper: %s' % (lower, upper))
+
+outliers = df[(df['Ratio'] < lower) | (df['Ratio'] > upper)]
+
+print(f'Outliers: {outliers}')
+
+described = df['Ratio'].describe()
+print('\n\nRatio collumn described: \n' + str(described))
+
+all_described = df.describe()
+print('\n\nWhole thing described: \n' + str(all_described))
